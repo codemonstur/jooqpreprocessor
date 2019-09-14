39c11cbce3d44f2b51fa1490b5cfeb9b214685e7
JurgenNED <jegvoorneveld@gmail.com>
2019-09-14 16:13:29 +0200

    Commit
    Fixed bug with default bit value, added removal of unique keys to create table statement

---
+++ src/test/java/TestJooqGenerator.java
@@ -0,0 +1,46 @@
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.jooq.meta.jaxb.Logging.FATAL;
+
+public class TestJooqGenerator {
+
+    public static void main(final String... args) throws Exception {
+        generateJooqCode("src/test/resources/set1/V1.11__duplicate-unique-key.sql", "target/generate-sources");
+    }
+
+    private static Path generateJooqCode(final String inputScript, final String outputDir) throws Exception {
+        GenerationTool.generate(newJooqConfiguration(inputScript, outputDir));
+        return Paths.get(outputDir);
+    }
+
+    private static Configuration newJooqConfiguration(final String scriptToParse, final String outputDirectory) {
+        return new Configuration()
+            .withLogging(FATAL)
+            .withGenerator(new Generator()
+                .withGenerate(new Generate()
+                    .withJavadoc(false)
+                    .withComments(false)
+                    .withEmptySchemas(false)
+                    .withEmptyCatalogs(false)
+                    .withGeneratedAnnotation(false)
+                    .withGlobalCatalogReferences(false)
+                    .withDaos(false)
+                    .withPojos(false)
+                    .withIndexes(false)
+                    .withKeys(false)
+                    .withRecords(false))
+                .withDatabase(new Database()
+                    .withName("org.jooq.meta.extensions.ddl.DDLDatabase")
+                    .withProperties(new Property()
+                        .withKey("scripts")
+                        .withValue(scriptToParse)))
+                .withTarget(new Target()
+                    .withPackageName("codegen")
+                    .withDirectory(outputDirectory)));
+    }
+
+}
